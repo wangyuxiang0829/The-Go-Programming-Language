b4d5410e7ebf10cc4dc266938c904dee5886a40e
wangyuxiang <yuxiangwang0829@gmail.com>
2019-06-07 16:16:48 +0800

    Update: Exercise 1.2

---
+++ ch1/1.2/main.go
@@ -0,0 +1,13 @@
+// Modify the echo program to print the index and value of each of its arguments, one per line.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	for i, v := range os.Args[1:] {
+		fmt.Println("index:", i, ", value:", v)
+	}
+}
